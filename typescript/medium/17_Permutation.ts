85f267219a67e495ca2bb3b8050f1e7056cf1a85
Vlad Vlasenko <kinzazdev579@gmail.com>
2023-08-26 19:29:02 +0300

    typescript challenge

---
+++ typescript/medium/17_Permutation.ts
@@ -0,0 +1,10 @@
+type Permutation<T, C = T> = [T] extends [never]
+  ? []
+  : C extends infer U
+  ? [U, ...Permutation<Exclude<T, U>>]
+  : [T];
+
+type perm = Permutation<"A" | "B" | "C">;
+// ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
+
+export {};
